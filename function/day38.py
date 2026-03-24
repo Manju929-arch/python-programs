aab02be01d3472862605b0568756cc7afd819456
Manjunath G <manjunathmanjuu155@gmail.com>
2026-03-24 11:44:00 +0530

    day 38 python program

---
+++ function/day38.py
@@ -0,0 +1,34 @@
+#
+# operator overloading
+#
+# __add__()
+# __sub__()
+# __mul__()
+# __div__()
+# __truedev__()
+# __lt__()
+# __gt__()
+# __le__()
+# __eq__()
+
+
+# class Employee:
+#     def __init__(self,name):
+#         self.name = name
+# e1 = Employee("john")
+# print(e1)
+# <__main__.Employee object at 0x0000018F19728590>
+# class student:
+#     def __init__(self,fees):
+#         self.fees =fees
+#     def __add__(self, other):
+#         return student(self.fees+other.fees)
+#     def __str__(self):
+#         return str(self.fees)
+#
+# s1 = student(100000)
+# s2 = student(75000)
+# s3 = student(200000)
+# s4 = student(30000)
+# print(s1+s2+s3+s4)
+
