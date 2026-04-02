40fe7acc28a0a9f1e415ea81b2f64c0f2eec2c40
Manjunath G <manjunathmanjuu155@gmail.com>
2026-04-02 12:37:59 +0530

    day 43 python program

---
+++ function/python programs/day43p.py
@@ -0,0 +1,71 @@
+# # # list and it properties
+# # li=[]
+# # li.append(10)
+# # li.append(20)
+# # li.append(30)
+# # li.append(40)
+# # li.append("dcl")
+# # print(li)
+# # li[0]=99
+# # print(li)
+# # # traversel
+# # for i in range(0,len(li)):
+# #     print(li[i])
+# #
+# # print("_______________")
+# #
+# # for ele in li:
+# #     print(ele)
+# #
+# # print("-=====================")
+# #
+# # for ele in li:
+# #     print(ele)
+# #
+#
+# l = [10,20,30,40,50]
+# l.append("dhee")
+# print(l)
+# l.remove("dhee")
+# print(l)
+# l.insert(1,80)
+# print(l)
+# l.pop()
+# print(l)
+# l.sort()
+# print(l)
+# l.sort(reverse=True)
+# print(l)
+# l.pop(1)
+# print(l)
+# l.clear()
+# print(l)
+# l.append(44)
+# l.append(56)
+# l.append(25)
+# l.append(78)
+# l.append(56)
+# l.append(90)
+# print(l)
+# l.copy()
+# print(l)
+
+# # WAP a program ot reverse
+# if __name__ == '__main__':
+#     li = [10,20,30,40,50,60]
+#     res =[]
+#     for i in range(len(li)-1,-1,-1):
+#         res.append(li[i])
+#     print(res)
+# # output [60, 50, 40, 30, 20, 10]
+
+# # WAP to print the sum of all the elemments from the given array list
+# if __name__ == '__main__':
+#     li = [10,20,30,40,50]
+#     sum =0
+#     for ele in li:
+#         sum = ele+sum
+#     print(sum)
+# # output 150
+
+
