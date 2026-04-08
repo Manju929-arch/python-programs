89aed2b1b9025704c9ca6df608d34196825c5216
Manjunath G <manjunathmanjuu155@gmail.com>
2026-04-08 21:04:29 +0530

    day 46 programs

---
+++ Practice program/day46p.py
@@ -0,0 +1,43 @@
+# binary search
+# def binarysearch(arr,key):
+#     start = 0
+#     end =   len(arr)-1
+#     while start<=end:
+#         mid = (start+end)//2
+#         if arr[mid] == key:
+#             return mid
+#         elif key<arr[mid]:
+#             end = mid-1
+#         else:
+#             start = mid+1
+#     return -1
+# if __name__ == '__main__':
+#     arr = [2,4,6,8,12,24,32]
+#     key = 8
+#     print(binarysearch(arr,key))
+# Bubble sort
+
+# def bobble(arr):
+#     for i in range(0,len(arr)):
+#         for j in range(0,len(arr)-1):
+#             if arr[j]>arr[j+1]:
+#                 arr[j],arr[j+1]= arr[j+1],arr[j]
+#     print(arr)
+# if __name__ == '__main__':
+#     arr =[32,19,21,7,3,2]
+#     bobble(arr)
+
+
+def compare_arr(arr,brr):
+    if len(arr)!= len(brr):
+        return False
+    for i in range(len(arr)):
+        if arr[i] != brr[i]:
+            return False
+        else:
+            continue
+    return  False
+if __name__ == '__main__':
+    arr = [12,32,10,24,45]
+    brr = [12, 32, 10, 24, 45]
+    print(compare_arr(arr,brr))
