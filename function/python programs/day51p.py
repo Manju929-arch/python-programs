451ba06c7d2939798874cc7d942717c6583420eb
Manjunath G <manjunathmanjuu155@gmail.com>
2026-04-15 11:22:56 +0530

    day 52 python programs

---
+++ function/python programs/day51p.py
@@ -0,0 +1,37 @@
+# if __name__ == '__main__':
+#     arr = [ 12,23,23,12,23,40,67]
+#     dict ={}
+#     for key in arr:
+#         if key in dict:
+#             dict[key]= dict[key]+1
+#         else:
+#             dict[key]=1
+#     print(dict)
+#     for key in dict:
+#         if dict[key]>1:
+#             print(key)
+from os import remove
+
+# if __name__ == '__main__':
+#     arr= [ 12,32,45,67,67,89,89,34,21,12,34]
+#     dict ={}
+#     for key in arr:
+#         if key in dict:
+#             dict[key] = dict[key]+1
+#
+#         else:
+#             dict[key]= 1
+#     for key in dict:
+#         if dict[key]>1:
+#             print(key)
+
+#
+# if __name__ == '__main__':
+#     arr = [12, 32, 45, 67, 67, 89, 89, 34, 21, 12, 34]
+#     res = []
+#     for ele in arr:
+#         if ele not in res:
+#             res.append(ele)
+#     print(res)
+
+
